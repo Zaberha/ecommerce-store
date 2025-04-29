716b00353580756992e1622cb3c2f68f621eeee1
Zaberha <albara.bitar@gmail.com>
2025-04-29 22:10:55 +0300

    Add files via upload

---
+++ languages/ar.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "welcome" => "مرحبًا بك في متجرنا!",
+    "cart" => "سلة التسوق",
+    "checkout" => "الدفع",
+    "language" => "اللغة",
+    "change_language" => "تغيير اللغة",
+    "E-Commerce Store" => "المتجر الإلكتروني",
+    "Home" => "الرئيسية",
+    "Trending" => "رائج",
+];
+?>
---
+++ languages/en.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "welcome" => "Welcome to our store!",
+    "cart" => "Shopping Cart",
+    "checkout" => "Checkout",
+    "language" => "Language",
+    "change_language" => "Change Language",
+    "E-Commerce Store" => "E-Commerce Store",
+    "Home" => "Home",
+    "Trending" => "Trending",
+];
+?>
